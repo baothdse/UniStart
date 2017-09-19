27e9ec99fee2bcc49fbedfb638cf9d07ae5fae1d
VANNNTSE61626\VanNNT <vannnt2112@gmail.com>
2017-09-19 07:23:36 +0700

    add class CORS filter and change @RequestParam to @RequestBody in Usercontroller

--- src/main/java/com/unistart/controller/UserController.java
+++ src/main/java/com/unistart/controller/UserController.java
@@ -3,6 +3,7 @@ package com.unistart.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,27 +26,29 @@ public class UserController {
 	private ErrorNotification error;
 	
 	@RequestMapping(value = UrlConstant.REGISTER, method = RequestMethod.POST)
-	public ResponseEntity<?> register(@RequestParam(ParamConstant.USERNAME) String username,
-									@RequestParam(ParamConstant.PASSWORD) String password,
-									@RequestParam(ParamConstant.EMAIL) String email) {
+	public ResponseEntity<?> register(@RequestBody Users user) {
+		String username = user.getUsername();
+		String password = user.getPassword();
+		String email = user.getEmail();
 		boolean isSuccess = userService.register(username, password, email);
 		if (isSuccess) {
 			return new ResponseEntity<Boolean> (isSuccess, HttpStatus.OK);
 		} else {
 			error = new ErrorNotification(ErrorConstant.ERR001, ErrorConstant.MES001);
-			return new ResponseEntity<ErrorNotification> (error, HttpStatus.OK);
+			return new ResponseEntity<ErrorNotification> (error, HttpStatus.FORBIDDEN);
 		}
 	}
 	
 	@RequestMapping(value = UrlConstant.CHECK_LOGIN, method = RequestMethod.POST)
-	public ResponseEntity<?> checkLogin(@RequestParam(ParamConstant.USERNAME) String username,
-										@RequestParam(ParamConstant.PASSWORD) String password) {
+	public ResponseEntity<?> checkLogin(@RequestBody Users u) {
+		String username = u.getUsername();
+		String password = u.getPassword();
 		Users user = userService.checkLogin(username, password);
 		if (user != null) {
 			return new ResponseEntity<Users> (user, HttpStatus.OK);
 		} else {
 			error = new ErrorNotification(ErrorConstant.ERR002, ErrorConstant.MES002);
-			return new ResponseEntity<ErrorNotification> (error, HttpStatus.OK);
+			return new ResponseEntity<ErrorNotification> (error, HttpStatus.CONFLICT);
 		}
 	}
 }
