e66af18ad0f823410e7b7bb97635e1b1dae3d713
baothdse <baothdse@gmail.com>
2017-09-20 12:49:43 +0700

    Add Generate Value annotation

--- src/main/java/com/unistart/entities/Users.java
+++ src/main/java/com/unistart/entities/Users.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -63,7 +65,7 @@ public class Users implements java.io.Serializable {
 	}
 
 	@Id
-
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "Id", unique = true, nullable = false)
 	public int getId() {
 		return this.id;
