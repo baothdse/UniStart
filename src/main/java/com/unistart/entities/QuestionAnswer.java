ee85f64b2e983c4f7c76c09d1e980fb6d140e7bf
VANNNTSE61626\VanNNT <vannnt2112@gmail.com>
2017-10-31 14:34:48 +0700

    create table

--- src/main/java/com/unistart/entities/QuestionAnswer.java
+++ src/main/java/com/unistart/entities/QuestionAnswer.java
@@ -11,140 +11,114 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-@Table(name = "Article", schema = "dbo", catalog = "University")
-public class Article implements java.io.Serializable {
+@Table(name = "Question_Answer", schema = "dbo", catalog = "University")
+public class QuestionAnswer implements java.io.Serializable {
 	private Integer id;
-	private String code;
 	private String title;
-	private String description;
-	private Date createDate;
-	private String contents;
-	private String image;
+	private String content;
+	private Integer vote;
+	private Integer type;
+	private Integer count;
+	private Users users;
 	private University university;
 	private Boolean isActive;
-	
-	
-	public Article() {
-		super();
-	}
-
-
-	public Article(Integer id, String code, String title, String description, String contents, String image, Date createDate,
+	public QuestionAnswer(Integer id, String title, String content, Integer vote, Integer type, Users users,
 			University university, Boolean isActive) {
 		super();
 		this.id = id;
-		this.code = code;
 		this.title = title;
-		this.description = description;
-		this.contents = contents;
-		this.image = image;
-		this.createDate = createDate;
+		this.content = content;
+		this.vote = vote;
+		this.type = type;
+		this.users = users;
 		this.university = university;
 		this.isActive = isActive;
 	}
-
+	public QuestionAnswer() {
+		super();
+	}
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "Id", unique = true, nullable = false)
 	public Integer getId() {
 		return id;
 	}
-
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-	@Column(name = "Code", nullable = false)
-	public String getCode() {
-		return code;
-	}
-
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
 	@Column(name = "Title", nullable = false)
 	public String getTitle() {
 		return title;
 	}
-
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-	@Column(name = "Description")
-	public String getDescription() {
-		return description;
-	}
-
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	@Temporal(TemporalType.DATE)
-	@Column(name = "createDate", length = 100)
-	public Date getCreateDate() {
-		return createDate;
-	}
-
-
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
-	}
-
-
+	
 	@Column(name = "Contents", nullable = false)
-	public String getContents() {
-		return contents;
+	public String getContent() {
+		return content;
 	}
-
-
-	public void setContents(String contents) {
-		this.contents = contents;
+	public void setContent(String content) {
+		this.content = content;
 	}
-
-	@Column(name = "Image")
-	public String getImage() {
-		return image;
+	
+	@Column(name = "Vote")
+	public Integer getVote() {
+		if(vote == null){
+			setVote(0);
+		}
+		return vote;
 	}
-
-
-	public void setImage(String image) {
-		this.image = image;
+	public void setVote(Integer vote) {
+		this.vote = vote;
 	}
-
+	
+	@Column(name = "Count", nullable = false)
+	public Integer getCount() {
+		return count;
+	}
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	@Column(name = "Type", nullable = false)
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "UserId")
+	public Users getUsers() {
+		return users;
+	}
+	public void setUsers(Users users) {
+		this.users = users;
+	}
+	
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "UniversityId")
 	public University getUniversity() {
 		return university;
 	}
-
-
 	public void setUniversity(University university) {
 		this.university = university;
 	}
-
-	@Column(name = "IsActive")
+	
+	@Column(name = "IsActive", nullable = false)
 	public Boolean getIsActive() {
 		return isActive;
 	}
-
-
 	public void setIsActive(Boolean isActive) {
 		this.isActive = isActive;
 	}
-	
 	
 	
 }
